f2c3624c177f666d059feddbb11f7866f2bc4875
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-02-04 21:27:02 -0500

    Creating full crud for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto, EditUserDto } from './dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) userId: number): Promise<User> {
+    return this.usersService.getUserById(userId);
+  }
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  createUser(@Body() dto: CreateUserDto): Promise<User> {
+    return this.usersService.createUser(dto);
+  }
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() dto: EditUserDto,
+  ) {
+    return this.usersService.updateUser(userId, dto);
+  }
+  @Delete(':id')
+  deleteUserById(@Param('id', ParseIntPipe) userId: number) {
+    return this.usersService.deleteUserById(userId);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto, EditUserDto } from './dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async getUsers() {
+    const users = await this.userRepository.find({});
+    return users;
+  }
+
+  async getUserById(userId: number) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    return user;
+  }
+
+  async createUser(dto: CreateUserDto) {
+    const newUser = this.userRepository.create(dto);
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+
+  async updateUser(userId: number, dto: EditUserDto) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new ForbiddenException('user not found');
+    return await this.userRepository.update({ id: userId }, { ...dto });
+  }
+
+  async deleteUserById(userId: number) {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new ForbiddenException('user not found');
+    return await this.userRepository.delete({ id: userId });
+  }
+}
