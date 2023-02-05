52a04e3a0501444319aeb724b5965340597aeea1
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-02-04 21:59:30 -0500

    Refactor service and adding HttpStatus messages

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto, EditUserDto } from './dto';
@@ -16,10 +21,16 @@ export class UsersService {
 
   async getUserById(userId: number) {
     const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) throw new HttpException('user not found', HttpStatus.NOT_FOUND);
     return user;
   }
 
   async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.findOne({
+      where: { username: dto.username },
+    });
+    if (user)
+      throw new HttpException('user already exists', HttpStatus.CONFLICT);
     const newUser = this.userRepository.create(dto);
     await this.userRepository.save(newUser);
     return newUser;
@@ -28,12 +39,13 @@ export class UsersService {
   async updateUser(userId: number, dto: EditUserDto) {
     const user = await this.userRepository.findOne({ where: { id: userId } });
     if (!user) throw new ForbiddenException('user not found');
-    return await this.userRepository.update({ id: userId }, { ...dto });
+    return await this.userRepository.save({ ...user, ...dto });
   }
 
   async deleteUserById(userId: number) {
-    const user = await this.userRepository.findOne({ where: { id: userId } });
-    if (!user) throw new ForbiddenException('user not found');
+    const result = await this.userRepository.delete({ id: userId });
+    if (result.affected === 0)
+      throw new HttpException('user not found', HttpStatus.NOT_FOUND);
     return await this.userRepository.delete({ id: userId });
   }
 }
